321aa4fc71248e8b9cabe81ef137507a97a19953
#Suyghur <suyghurmjp@outlook.com>
2023-03-31 16:38:19 +0800

    feat(v0.0.1): project initialization

---
+++ app/src/main/java/com/yyxx/kvdb/KVDBApplication.java
@@ -0,0 +1,10 @@
+package com.yyxx.kvdb;
+
+import android.app.Application;
+
+/**
+ * @author #Suyghur.
+ * Created on 2023/3/31
+ */
+public class KVDBApplication extends Application {
+}
---
+++ library_kvdb/src/main/java/cn/yyxx/kvdb/KVDB.java
@@ -0,0 +1,8 @@
+package cn.yyxx.kvdb;
+
+/**
+ * @author #Suyghur.
+ * Created on 2023/3/31
+ */
+public class KVDB {
+}
